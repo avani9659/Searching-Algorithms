55ed3581de55226dbb3773dbc7531f6b3b02c45a
avani9659 <avani9659@gmail.com>
2022-09-12 20:55:40 +0530

    Adding comments

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,15 @@ public class Main {
         linearSearch(arr, 10);
     }
 
+    /**
+     * Linear search algorithm. Here the element is searched by going through the array elements one by one.
+     * If the element is found, loop is terminated.
+     * The time complexity for this algorithm is O(N). (worst case scenario)
+     * Best case here would be - if the element is found in the start of array.
+     * Worst case here would be - if the element is at the end of the array or the element is not present in array.
+     * @param arr The given array
+     * @param element The element to find
+     */
     public static void linearSearch(int[] arr, int element){
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == element){
